ee1c4a52060d2a880672b0a5ca27225d47ec9d62
Sypth <acamaddo@spcf.edu.ph>
2025-04-10 08:50:22 +0800

    🔧 Automation conditions & user migration

---
+++ database/migrations/2025_04_08_071655_create_automation_conditions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('automation_conditions', function (Blueprint $table) {
+            $table->id();
+            $table->string('icon');
+            $table->string('name');
+            $table->string('description');
+            $table->enum('type', ['emotion', 'time', 'device']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('automation_conditions');
+    }
+};
