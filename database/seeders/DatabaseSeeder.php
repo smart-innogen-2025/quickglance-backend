4a5e2df5a309214798f489f098cbba1104427b69
Sypth <acamaddo@spcf.edu.ph>
2025-04-10 08:51:12 +0800

    🔧 Including the AutomationCondition seeder from the database seeder file.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // Call the UserSeeder
         $this->call([
             UserSeeder::class,
-            CategoryActionSeeder::class
+            CategoryActionSeeder::class,
+            AutomationConditionSeeder::class,
         ]);
     }
 }
