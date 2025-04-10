d0ce9d9433b675a703f967e71887c12083a4d558
Sypth <acamaddo@spcf.edu.ph>
2025-04-10 14:47:32 +0800

    🔧 Adjusted the fields of personal_access_token table to sync with the register method in Auth Controller.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Spatie\Permission\Models\Role;
+use App\Models\Role;
 
 class AuthController extends Controller
 {
