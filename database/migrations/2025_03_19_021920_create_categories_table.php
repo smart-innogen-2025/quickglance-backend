dffe450cb951758482c494c6043cc3b838f6adc5
Sypth <acamaddo@spcf.edu.ph>
2025-04-11 15:50:38 +0800

    🔧 making image field nullable on categories table

--- database/migrations/2025_03_19_021920_create_categories_table.php
+++ database/migrations/2025_03_19_021920_create_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->string('name');
             $table->string('icon')->nullable();
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
