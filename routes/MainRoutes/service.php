515f05320ce2889091f79c83cd9c0e254662e17b
Sypth <acamaddo@spcf.edu.ph>
2025-04-23 10:24:27 +0800

    🔧 Making services index route api to be public

--- routes/MainRoutes/service.php
+++ routes/MainRoutes/service.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ServiceController;
 
 
-Route::prefix('service')->middleware(['auth:sanctum', 'role:service-admin'])->group(function () {
+Route::prefix('service')->group(function () {
     Route::get('/', [ServiceController::class, 'index']);
-    Route::post('/', [ServiceController::class, 'store']);
+    Route::post('/', [ServiceController::class, 'store'])->middleware(['auth:sanctum', 'role:service-admin']);
 });
